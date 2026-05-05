d2222445da46bfc22314eedb5b87acdda23f22ba
CaelanWR <caelanwilkierogers@gmail.com>
2026-05-05 16:20:17 -0400

    Improve hosted API connection errors

--- site/config.js
+++ site/config.js
@@ -1,4 +1,4 @@
 // Optional protected API location. Leave blank to let users enter it in the UI.
 // Example:
 // window.OUTCOMES_API_URL = "https://college-outcomes-api.example.com";
-window.OUTCOMES_API_URL = "";
+window.OUTCOMES_API_URL = "https://college-outcomes-api.onrender.com";
